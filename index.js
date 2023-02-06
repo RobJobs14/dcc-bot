74b3157b92ec846e9339ece0e8096042e033ec2b
ROBJOBS <zexi2004@gmail.com>
2023-02-06 00:11:18 -0500

    dirname

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ const eventsPath = path.join(__dirname, "events");
 const eventFiles = fs
   .readdirSync(eventsPath)
   .filter((file) => file.endsWith(".js"));
+console.log(__dirname);
 
 for (const file of eventFiles) {
   const filePath = path.join(eventsPath, file);
