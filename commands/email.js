18bae0762d218fbb262ead8feb0c48ac62484665
ROBJOBS <zexi2004@gmail.com>
2023-02-06 14:15:31 -0500

    nolog

--- commands/email.js
+++ commands/email.js
@@ -5,7 +5,6 @@ module.exports = {
     .setName("email")
     .setDescription("Get the dawson chess club email"),
   async execute(interaction) {
-    console.log(interaction);
     await interaction.reply({
       content: "chessandgoclub@dawsonstudentunion.com",
       ephemeral: true,
--- commands/invite.js
+++ commands/invite.js
@@ -6,7 +6,6 @@ module.exports = {
     .setName("invite")
     .setDescription("Get the invite link to this server"),
   async execute(interaction) {
-    console.log(interaction);
     await interaction.deferReply();
     await wait(4000);
     await interaction.editReply({
