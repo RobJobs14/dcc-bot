18056a9571c069b266e9b1f0a5fef184df461203
ROBJOBS <zexi2004@gmail.com>
2023-02-06 18:44:50 -0500

    test

--- commands/invite.js
+++ commands/invite.js
@@ -1,14 +1,12 @@
 const { SlashCommandBuilder } = require("discord.js");
-const wait = require("node:timers/promises").setTimeout;
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("invite")
     .setDescription("Get the invite link to this server"),
   async execute(interaction) {
-    await interaction.deferReply();
-    await wait(4000);
-    await interaction.editReply({
+    console.log(interaction);
+    await interaction.reply({
       content: "https://discord.gg/FQvunv4PAX",
       ephemeral: true,
     });
