483e679a131db7b3e940c4028a104165d0a60d47
ROBJOBS <zexi2004@gmail.com>
2023-02-16 02:41:19 -0500

    gif check

--- events/gameListener.js
+++ events/gameListener.js
@@ -1,8 +1,6 @@
 const { Events } = require("discord.js");
 const ChessWebAPI = require("chess-web-api");
 
-const { LICHESS_TOKEN: litoken } = process.env;
-
 module.exports = {
   name: Events.MessageCreate,
   once: false,
@@ -17,25 +15,43 @@ module.exports = {
           method: "POST",
           body: gameID,
           headers: {
-            Authorization: `Bearer ${litoken}`,
+            Authorization: `Bearer lip_L8kSkfR9bL7b0hi2Gdxr`,
             "Content-Type": "application/x-www-form-urlencoded",
           },
         })
           .then((response) => response.json())
           .then((gameID) => {
             const id = gameID.id;
-            message.channel.send(
-              `https://lichess1.org/game/export/gif/${id}.gif?theme=brown&piece=cburnett`
-            );
+            const gifURL = `https://lichess1.org/game/export/gif/${id}.gif?theme=brown&piece=cburnett`;
+            fetch(gifURL)
+              .then((response) => {
+                if (response.ok) {
+                  message.channel.send(gifURL);
+                } else {
+                  console.error(
+                    `Failed to fetch GIF: ${response.status} ${response.statusText}`
+                  );
+                }
+              })
+              .catch((error) => console.error(error));
           })
           .catch((error) => console.error(error));
       });
     } else if (message.content.includes("https://lichess.org/")) {
       const startIndex = message.content.indexOf("https://lichess.org/") + 20;
       const id = message.content.substring(startIndex, startIndex + 8);
-      message.channel.send(
-        `https://lichess1.org/game/export/gif/${id}.gif?theme=brown&piece=cburnett`
-      );
+      const gifURL = `https://lichess1.org/game/export/gif/${id}.gif?theme=brown&piece=cburnett`;
+      fetch(gifURL)
+        .then((response) => {
+          if (response.ok) {
+            message.channel.send(gifURL);
+          } else {
+            console.error(
+              `Failed to fetch GIF: ${response.status} ${response.statusText}`
+            );
+          }
+        })
+        .catch((error) => console.error(error));
     }
   },
 };
