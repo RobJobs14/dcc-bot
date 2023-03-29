ed2a61f2459f6b632f5308fd699bd60ea528b5fc
ROBJOBS <zexi2004@gmail.com>
2023-03-29 19:42:02 -0400

    status update

--- events/ready.js
+++ events/ready.js
@@ -5,11 +5,8 @@ module.exports = {
   once: true,
   execute(client) {
     console.log(`Ready! Logged in as ${client.user.tag}`);
-    client.user.setActivity(
-      "discord break my code so some commands will not work",
-      {
-        type: ActivityType.Watching,
-      }
-    );
+    client.user.setActivity("/profile set", {
+      type: ActivityType.Playing,
+    });
   },
 };
