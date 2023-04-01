71c9793e4fa331735c93e4bc3dde1e498917c4b4
ROBJOBS <zexi2004@gmail.com>
2023-04-01 17:56:15 -0400

    update

--- commands/database.js
+++ commands/database.js
@@ -10,6 +10,15 @@ module.exports = {
         .setDescription("Search the masters database")
         .addStringOption((option) =>
           option
+            .setName("type")
+            .setRequired(true)
+            .addChoices(
+              { name: "opening", value: "opening" },
+              { name: "game", value: "game" }
+            )
+        )
+        .addStringOption((option) =>
+          option
             .setName("fen")
             .setDescription("The FEN string to search or the starting position")
         )
@@ -67,56 +76,64 @@ module.exports = {
           return {};
         });
 
-      const openingEmbed = new EmbedBuilder()
-        .setColor(0xdbc300)
-        .setTitle("Masters Database")
-        .setDescription(
-          `${apiResponse.opening.eco} ${apiResponse.opening.name}`
+      const type = interaction.options.getString("type");
+
+      if (type === "opening") {
+        const mastersEmbed = new EmbedBuilder()
+          .setColor(0xdbc300)
+          .setTitle("Masters Database")
+          .setDescription(
+            `${apiResponse.opening.eco} ${apiResponse.opening.name}`
+          );
+
+        apiResponse.moves.forEach((move) => {
+          const totalGames = move.white + move.draws + move.black;
+          const whiteWinRate = ((move.white / totalGames) * 100).toFixed(1);
+          const drawRate = ((move.draws / totalGames) * 100).toFixed(1);
+          const blackWinRate = ((move.black / totalGames) * 100).toFixed(1);
+
+          mastersEmbed.addFields({
+            name: "\u200B",
+            value: `${move.san}: ${totalGames} games ${whiteWinRate}% / ${drawRate}% / ${blackWinRate}%`,
+          });
+        });
+
+        const totalGames =
+          apiResponse.white + apiResponse.draws + apiResponse.black;
+        const whiteWinRate = ((apiResponse.white / totalGames) * 100).toFixed(
+          1
+        );
+        const drawRate = ((apiResponse.draws / totalGames) * 100).toFixed(1);
+        const blackWinRate = ((apiResponse.black / totalGames) * 100).toFixed(
+          1
         );
 
-      apiResponse.moves.forEach((move) => {
-        const totalGames = move.white + move.draws + move.black;
-        const whiteWinRate = ((move.white / totalGames) * 100).toFixed(1);
-        const drawRate = ((move.draws / totalGames) * 100).toFixed(1);
-        const blackWinRate = ((move.black / totalGames) * 100).toFixed(1);
-
-        openingEmbed.addFields({
+        mastersEmbed.addFields({
           name: "\u200B",
-          value: `${move.san}: ${totalGames} games ${whiteWinRate}% / ${drawRate}% / ${blackWinRate}%`,
+          value: `Σ: ${totalGames} games ${whiteWinRate}% / ${drawRate}% / ${blackWinRate}%`,
         });
-      });
+      } else if (type === "game") {
+        const mastersEmbed = new EmbedBuilder()
+          .setColor(0xdbc300)
+          .setTitle("Top Games");
 
-      const totalGames =
-        apiResponse.white + apiResponse.draws + apiResponse.black;
-      const whiteWinRate = ((apiResponse.white / totalGames) * 100).toFixed(1);
-      const drawRate = ((apiResponse.draws / totalGames) * 100).toFixed(1);
-      const blackWinRate = ((apiResponse.black / totalGames) * 100).toFixed(1);
+        apiResponse.topGames.forEach((game) => {
+          const whitePlayer = `${game.white.name}`;
+          const blackPlayer = `${game.black.name}`;
+          let result;
+          if (game.winner === "white") result = "1-0";
+          else if (game.winner === "black") result = "0-1";
+          else result = "1/2-1/2";
 
-      openingEmbed.addFields({
-        name: "\u200B",
-        value: `Σ: ${totalGames} games ${whiteWinRate}% / ${drawRate}% / ${blackWinRate}%`,
-      });
-
-      const topGamesEmbed = new EmbedBuilder()
-        .setColor(0xdbc300)
-        .setTitle("Top Games");
-
-      apiResponse.topGames.forEach((game) => {
-        const whitePlayer = `${game.white.name}`;
-        const blackPlayer = `${game.black.name}`;
-        let result;
-        if (game.winner === "white") result = "1-0";
-        else if (game.winner === "black") result = "0-1";
-        else result = "1/2-1/2";
-
-        topGamesEmbed.addFields({
-          name: "\u200B",
-          value: `${game.uci} [${whitePlayer} - ${blackPlayer}](https://lichess.org/${game.id}) *${game.month}* · ${result}`,
+          mastersEmbed.addFields({
+            name: "\u200B",
+            value: `${game.uci} [${whitePlayer} - ${blackPlayer}](https://lichess.org/${game.id}) *${game.month}* · ${result}`,
+          });
         });
-      });
+      }
 
       // Send the embeds
-      interaction.reply({ embeds: [openingEmbed, topGamesEmbed] });
+      interaction.reply({ embeds: [mastersEmbed] });
     } else if (subcommand === "lichess") {
       const fen = interaction.options.getString("fen");
       const pgn = interaction.options.getString("pgn");
